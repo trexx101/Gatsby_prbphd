90032e1f97fb39ad3df51286b71fc2330a6de3a0
Inah Afen <inahafen@gmail.com>
2019-04-30 18:53:39 +0800

    infinity testimonials

--- src/components/Slider/SimpleSlider.js
+++ src/components/Slider/SimpleSlider.js
@@ -45,13 +45,13 @@ class SimpleSlider extends React.Component {
       infinite: true,
       speed: 500,  
       slidesToShow: 2,
-      slidesToScroll: 2,
+      slidesToScroll: 1,
       responsive: [
         {
           breakpoint: 1024,
           settings: {
             slidesToShow: 2,
-            slidesToScroll: 2,
+            slidesToScroll: 1,
             infinite: true,
             dots: true
           }
@@ -60,7 +60,7 @@ class SimpleSlider extends React.Component {
           breakpoint: 600,
           settings: {
             slidesToShow: 1,
-            slidesToScroll: 4,
+            slidesToScroll: 1,
             initialSlide: 2
           }
         },
@@ -68,7 +68,7 @@ class SimpleSlider extends React.Component {
           breakpoint: 480,
           settings: {
             slidesToShow: 1,
-            slidesToScroll: 4
+            slidesToScroll: 1
           }
         }
       ]
